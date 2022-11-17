615ca3982dd2aa2737bfae4ffd040dcb6070473e
library-one <118429457+library-one@users.noreply.github.com>
2022-11-17 17:34:11 +0900

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import './style.css';
-
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <TextField label="Email Address" />
+      <TextField label="Password" type="password" />
+      <Checkbox value="remember" color="primary" />
+      <Button type="submit">Sign in</Button>
     </div>
   );
 }
