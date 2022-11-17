f62b1a9c9054d442f41b9b5ca9f435e275408c20
library-one <118429457+library-one@users.noreply.github.com>
2022-11-17 17:49:46 +0900

    1 file modified

--- App.tsx
+++ App.tsx
@@ -3,13 +3,38 @@ import './style.css';
 import TextField from '@mui/material/TextField';
 import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
+
 export default function App() {
   return (
     <div>
-      <TextField label="Email Address" />
-      <TextField label="Password" type="password" />
-      <Checkbox value="remember" color="primary" />
-      <Button type="submit">Sign in</Button>
+      <TextField 
+      label="Email Address" 
+      required 
+      fullWidth 
+      name="email"
+      autoComplete="email"
+      //autoFocus
+       />
+      <TextField
+        label="Password"
+        type="password"
+        required
+        fullWidth
+        name="password"
+        autoComplete="current-password"
+      />
+       <FormControlLabel
+        control={<Checkbox value="remember" 
+        color="primary" />}
+        label="Remember me"
+      />
+      <Button type="submit" fullwidth variant="contained"
+      sx={{mt:3,mb:2}}>
+
+      Sign in
+      </Button>
     </div>
   );
 }
